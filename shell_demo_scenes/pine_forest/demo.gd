2ea512f4321e9711ff5f00529df2122937c3d126
andras204 <85683781+andras204@users.noreply.github.com>
2023-11-06 19:34:45 +0100

    Pine forest converted to vertex extrude

--- shell_demo_scenes/pine_forest/demo.gd
+++ shell_demo_scenes/pine_forest/demo.gd
@@ -1,28 +1,44 @@
 @tool
 extends Node3D
 
-@export var shells: int = 256
-@export_range(0.0001, 1.0, 0.0001) var shell_spacing: float = 0.001
+@export_category("shell Positioning")
+@export var shell_count: int = 32
+@export_range(0.0001, 1.0, 0.0001) var shell_spacing: float = 0.01
+@export var shell_spacing_attenuation: float = 1.0
 
-@export var density: int = 32
+@export_category("Shell Texture")
+@export_range(1, 4096) var density: int = 32
+@export var noise_min: float = 0.0
+@export var noise_max: float = 1.0
 
-@export var shell_scene = preload("res://shell_demo_scenes/pine_forest/shell.tscn")
+
+var shell_scene = preload("res://shell_demo_scenes/pine_forest/shell.tscn")
 
 var shell_array: Array
 
+
+var time: float = 0.0
+
 func _ready():
-	for x in range(shells):
+	for x in range(shell_count):
 		var instance = shell_scene.instantiate()
-		instance.transform.origin = Vector3(0, x * shell_spacing, 0)
 		instance.name = "shell" + str(x)
-		instance.height = x as float / (shells - 1)
-		instance.shell_index = x
-		instance.density = density
+		update_shader_params(instance, x)
 		add_child(instance)
 		shell_array.append(instance)
 
 func _process(delta):
 	var i: int = 0
 	for s in shell_array:
-		s.transform.origin = Vector3(0, i * shell_spacing, 0)
+		update_shader_params(s, i)
 		i += 1
+
+func update_shader_params(instance, index: int):
+	instance.shell_index = index
+	instance.shell_count = shell_count
+	instance.shell_spacing = shell_spacing
+	instance.shell_spacing_attenuation = shell_spacing_attenuation
+	
+	instance.density = density
+	instance.noise_min = noise_min
+	instance.noise_max = noise_max
--- shell_demo_scenes/pine_forest/shell.gd
+++ shell_demo_scenes/pine_forest/shell.gd
@@ -1,11 +1,31 @@
 @tool
 extends MeshInstance3D
 
-@export_range(0, 1) var height: float = 0.5
+@export_category("shell Positioning")
 @export var shell_index: int = 1
+@export var shell_count: int = 2
+@export_range(0.0001, 1.0, 0.0001) var shell_spacing: float = 0.01
+@export var shell_spacing_attenuation: float = 1.0
+
+@export_category("Shell Texture")
 @export var density: int = 32
+@export var noise_min: float = 0.0
+@export var noise_max: float = 1.0
+
 
 func _ready():
-	mesh.material.set_shader_parameter("height", height)
+	update_shader_params()
+
+
+func _process(delta):
+	update_shader_params()
+
+
+func update_shader_params():
 	mesh.material.set_shader_parameter("shell_index", shell_index)
+	mesh.material.set_shader_parameter("shell_count", shell_count)
+	mesh.material.set_shader_parameter("shell_spacing", shell_spacing)
+	
 	mesh.material.set_shader_parameter("density", density)
+	mesh.material.set_shader_parameter("noise_min", noise_min)
+	mesh.material.set_shader_parameter("noise_max", noise_max)
